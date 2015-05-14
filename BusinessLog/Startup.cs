5a3a1486c6f6d0fd5be18511c1767a50658520df
halkang <dearlyhal@gmail.com>
2015-05-14 16:58:11 +0800

    第一次同步到GitHub

---
+++ BusinessLog/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BusinessLog.Startup))]
+namespace BusinessLog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
